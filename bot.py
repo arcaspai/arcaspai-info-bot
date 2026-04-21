9072639ba759fe9c9969f497cc8a6bb023a0675c
Redder <gayeong897@gmail.com>
2026-04-21 22:43:54 +0900

    embed footer changed the value

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+embed_footer = "Arcaspai Project © 2021-2026 Redder"
+
 TOKEN = tokenbox.token
 
 # start
@@ -26,7 +28,7 @@ async def introduct_embed(interaction) :
     embed.add_field(name="Project Start", value="2021-08-29", inline=True)
     embed.add_field(name="Project Reboot", value="2026-01-27", inline=True)
     embed.add_field(name="Website", value="https://arcaspai.github.io", inline=False)
-    embed.set_footer(text="Arcaspai Project © 2021-2026 Redder")
+    embed.set_footer(text=embed_footer)
 
     await interaction.response.send_message(embed=embed)
 
@@ -44,18 +46,19 @@ async def link_choice(interaction: discord.Interaction, site: app_commands.Choic
     await interaction.response.send_message(f"{site.value}")
 
 # send characters image
-@tree.command(name="portrait", description="check character portrait (input only small letter plase)")
+@tree.command(name="character", description="embed character images (input only small letter plase)")
 @app_commands.choices(imagetype=[
     app_commands.Choice(name="portrait", value="portrait"),
     app_commands.Choice(name="icon", value="icon")
 ])
-async def portraits_embed(interaction: discord.Interaction, character: str, imagetype: app_commands.Choice[str]) :
-    embed = discord.Embed(title=f"{character}'s portrait", colour=discord.Colour.from_rgb(144, 136, 255))
+async def characters_info(interaction: discord.Interaction, character: str, imagetype: app_commands.Choice[str]) :
+    embed = discord.Embed(title=f"{character}'s {imagetype.value}", colour=discord.Colour.from_rgb(144, 136, 255))
     embed.set_author(name="Arcaspaio", url="https://arcaspai.github.io", icon_url="https://media.discordapp.net/attachments/1491315989428572171/1493217429692747826/arcaspaibanner08.png")
-    embed.set_image(url=f"https://arcaspai.github.io/images/decoration/banner-{character}.png") # need fix to https://arcaspai.github.io/universe/_images/{imagetype}s/{character}_{imagetype}.png
-    embed.set_footer(text="Arcaspai Project © 2021-2026 Redder")
+    embed.set_image(url=f"https://arcaspai.github.io/images/decoration/banner-{character}.png") #need fix https://arcaspai.github.io/universe/_images/{imagetype}s/{character}_{imagetype}.png
+    embed.set_footer(text=embed_footer)
 
     await interaction.response.send_message(embed=embed)
+
 
 # running bot
 client.run(TOKEN)
